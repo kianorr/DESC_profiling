4e4fc8f0dd0fb667b4ad46fe9c8c9f9abbeca5a1
Kian Orr <orrk931@gmail.com>
2023-12-11 14:14:13 -0500

    followed singhs paper instead

--- faster_zernike.py
+++ faster_zernike.py
@@ -1,32 +1,34 @@
-from desc.grid import LinearGrid
+"""zernike algorithms."""
 import numpy as np
+from desc.grid import LinearGrid
 
-def zernike_radial_direct(r, n):
+
+def zernike_radial_singh(r, n):
     """Radial part of zernike polynomials. Modified Prata's algorithm."""
-    R = np.zeros((len(n), len(n), len(r)))  # R_nmk
+    R = np.full((len(n), len(n), len(r)), np.nan)
 
-    M = np.max(n)
-    for n_i in range(M):
+    n_max = np.max(n)
+    for n_i in range(n_max):
+        R[n_i, n_i] = r**n_i
 
-        R[n_i, n_i] = r ** n_i
-        print(f"{R[n_i, n_i]=}")
-
-        for m_i in range(M, 1, -2):
+    for n_i in range(2, n_max + 2, 2):
+        for m_i in range(0, n_max - n_i):
             K_1 = 2 * n_i / (m_i + n_i)
             K_2 = 1 - K_1
 
-            R[n_i, m_i] = r * K_1 * R[n_i - 1, m_i - 1] + K_2 * R[n_i - 2, m_i]
+            term_1 = r * K_1 * R[n_i + m_i - 1, abs(m_i - 1)]
+            term_2 = K_2 * R[m_i + n_i - 2, m_i]
 
-        # covering the case where n is even
-        if n_i % 2 == 0:
-            R[n_i, 0] = 2 * r * R[n_i - 1, 1] - R[n_i - 2, 0]
-    
+            R[n_i + m_i, m_i] = term_1 + term_2
+
     return R
 
+
 L = 8
+M = 8
 # TODO: add r without desc
-grid = LinearGrid(L=L)
+grid = LinearGrid(L=L, M=M)
 r = grid.nodes[:, 0]
 
-l = np.arange(L)
-print(zernike_radial_direct(r, l)[-1])
+_l = np.arange(L) + 1
+print(zernike_radial_singh(r, _l)[7, 5])
