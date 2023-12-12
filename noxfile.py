c82c1917d4748fe63c35552ad8199dab2723354a
Kian Orr <orrk931@gmail.com>
2023-12-11 21:16:48 -0500

    changed noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,6 @@ import nox
 @nox.session
 def tests(session: nox.Session) -> None:
     """Run the unit and regular tests. Testing."""
-    session.install("--upgrade", "pip", "setuptools")
+    session.install("--upgrade", "pip")
     session.install("pytest")
     session.run("pytest", *session.posargs)
