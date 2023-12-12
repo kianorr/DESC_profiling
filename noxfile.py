6ae62ad9a663b39ea2e3337cb7a2fa6d42b49b15
Kian Orr <orrk931@gmail.com>
2023-12-11 19:54:24 -0500

    try without version

--- noxfile.py
+++ noxfile.py
@@ -5,5 +5,5 @@ import nox
 @nox.session
 def tests(session: nox.Session) -> None:
     """Run the unit and regular tests."""
-    session.install(".[test]")
+    session.install("pytest")
     session.run("pytest", *session.posargs)
