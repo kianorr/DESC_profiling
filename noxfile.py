a01b0c69ecd51517155fdb042ab483079c8de858
Kian Orr <orrk931@gmail.com>
2023-12-11 20:07:28 -0500

    testing

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,6 @@ import nox
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    """Run the unit and regular tests."""
+    """Run the unit and regular tests. Testing."""
     session.install("pytest")
     session.run("pytest", *session.posargs)
