e39715ee4a1996514d780688719fa23511ccf9af
Kian Orr <orrk931@gmail.com>
2023-12-11 19:33:05 -0500

    install pytest directly

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,9 @@
+"""Use nox to run tests."""
 import nox
 
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    """
-    Run the unit and regular tests.
-    """
-    session.install(".[test]")
+    """Run the unit and regular tests."""
+    session.install("pytest>=7.0.0")
     session.run("pytest", *session.posargs)
